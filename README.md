e4879de5338ac8ac981b3b1518228e915689c521
HarshAndroid <rajpurohitharsh2020@gmail.com>
2024-11-06 13:38:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# social_media_feed
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/harshRajpurohit/social_media_feed/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.harshRajpurohit.social_media_feed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
